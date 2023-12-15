4b3d01f0327d32ae9f9384c659397223b61592f8
Julia Longtin <julia.longtin@gmail.com>
2023-12-15 00:56:10 +0000

    try Word32# for base 4.15.0 and later.

--- src/Data/Bits/Floating/Prim.hs
+++ src/Data/Bits/Floating/Prim.hs
@@ -28,18 +28,20 @@ module Data.Bits.Floating.Prim (
 import GHC.Exts (Double#, Double(D#), Float#, Float(F#))
 import GHC.Word (Word32(W32#), Word64(W64#))
 
-
 #if WORD_SIZE_IN_BITS == 64 && MIN_VERSION_base(4,17,0)
 -- The name of Word# changed to Word64# in ghc 9.4.1
 import GHC.Exts (Word64#, Word32#)
-
 #define WORD64 Word64
 #define WORD32 Word32
 #elif WORD_SIZE_IN_BITS == 64
 import GHC.Exts (Word#)
-
 #define WORD64 Word
+#if MIN_VERSION_base(4,15,0)
 #define WORD32 Word
+#else
+import GHC.Exts (Word32#)
+#define WORD32 Word32
+#endif
 #else
 #error non-X86_64 architectures not supported
 #endif
