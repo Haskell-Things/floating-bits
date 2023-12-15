0a142179941eb28366085beae5baca6f8e4d36bf
Julia Longtin <julia.longtin@gmail.com>
2023-12-15 15:35:15 +0000

    Ghc 9.4.6 (#5)
    * fixes for GHC 9.4.6.
    
    * support 64 bit systems older than ghc 9.4
    
    * add check for ghc 9.4.1+.
    
    * add check for 32 bit ghc 9.4.1+.
    
    * move definitions under copyright.
    
    * be explicit about imports, and a shot in the dark on base 4,16,0.
    
    * start a gitignore.
    
    * correctly import what we need from GHC.Exts.
    
    * fix spacing.
    
    * try Word32# for base 4.15.0 and later.
    
    * try Word32# for base 4.17.
    
    * revert prior.
    
    * reverse polarity of an if.
    
    * document GHC version changes.

--- src/Data/Bits/Floating/Prim.hs
+++ src/Data/Bits/Floating/Prim.hs
@@ -19,48 +19,47 @@
 
 #include "MachDeps.h"
 
-module Data.Bits.Floating.Prim where
+module Data.Bits.Floating.Prim (
+  double2WordBitwise,
+  float2WordBitwise,
+  word2DoubleBitwise,
+  word2FloatBitwise) where
 
-import GHC.Exts
-import GHC.Word
+import GHC.Exts (Double#, Double(D#), Float#, Float(F#))
+import GHC.Word (Word32(W32#), Word64(W64#))
 
-#if MIN_VERSION_base(4,17,0)
--- The name of Word# changed to Word64# in ghc 9.4.1
+#if WORD_SIZE_IN_BITS == 64 && MIN_VERSION_base(4,17,0)
+-- The name of Word# changed to Word64# in base 4.17.0 (GHC 9.4.1)
+import GHC.Exts (Word64#, Word32#)
 #define WORD64 Word64
 #define WORD32 Word32
-#else
+#elif WORD_SIZE_IN_BITS == 64
+-- Earlier than GHC 9.4? then Word is our 64 bit type.
+import GHC.Exts (Word#)
 #define WORD64 Word
+#if MIN_VERSION_base(4,15,0)
+-- Word32 and Word diverged in base 4.15.0 (GHC 9.0.1?))
+import GHC.Exts (Word32#)
+#define WORD32 Word32
+#else
+-- Earlier than GHC9? then use Word for both 32 and 64 bit FFI calls.
 #define WORD32 Word
 #endif
+#else
+#error non-X86_64 architectures not supported
+#endif
 
-#if WORD_SIZE_IN_BITS == 64
 foreign import prim "double2WordBwzh"
     double2WordBitwise# :: Double# -> WORD64#
 foreign import prim "word2DoubleBwzh"
     word2DoubleBitwise# :: WORD64# -> Double#
-#elif WORD_SIZE_IN_BITS == 32
-foreign import prim "double2WordBwzh"
-    double2WordBitwise# :: Double# -> WORD32#
-foreign import prim "word2DoubleBwzh"
-    word2DoubleBitwise# :: WORD32# -> Double#
-#else
-#error "Unsupported word size"
-#endif
+foreign import prim "float2WordBwzh"
+    float2WordBitwise# :: Float# -> WORD32#
+foreign import prim "word2FloatBwzh"
+    word2FloatBitwise# :: WORD32# -> Float#
 
 #undef WORD64
 #undef WORD32
-
-#if MIN_VERSION_base(4,15,0)
-foreign import prim "float2WordBwzh"
-    float2WordBitwise# :: Float# -> Word32#
-foreign import prim "word2FloatBwzh"
-    word2FloatBitwise# :: Word32# -> Float#
-#else
-foreign import prim "float2WordBwzh"
-    float2WordBitwise# :: Float# -> Word#
-foreign import prim "word2FloatBwzh"
-    word2FloatBitwise# :: Word# -> Float#
-#endif
 
 -- | Convert a 'Double' to a 'Word64' while preserving the bit-pattern.
 {-# INLINE double2WordBitwise #-}
